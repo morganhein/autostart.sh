291adb3796647ea7d8ef112011ca98f6f494c752
Morgan Hein <work@morganhe.in>
2022-01-06 12:17:28 -0800

    fix: runner mock and import

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
 package main
 
-////go:generate moq -out pkg/io/shell_mock.go pkg/io Runner
-//go:generate moq -out pkg/io/fs_mock.go pkg/io Filesystem
+//go:generate moq -out pkg/io/runner_mock.go pkg/io Runner
+//go:generate moq -out pkg/io/filesystem_mock.go pkg/io Filesystem
