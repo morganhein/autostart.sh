026f2f7c261e110a0986f046872417d03a182138
Morgan Hein <work@morganhe.in>
2022-08-26 12:02:10 -0700

    fix: fixed generation when moqs are bad

--- generate.go
+++ generate.go
@@ -1,4 +1,7 @@
 package main
 
+//go:generate rm -f pkg/out/runner_mock.go
+//go:generate rm -f pkg/io/filesystem_mock.go
+
 //go:generate moq -out pkg/io/runner_mock.go pkg/io Runner
 //go:generate moq -out pkg/io/filesystem_mock.go pkg/io Filesystem
